5b616b48626d23f9588ab4e5fa7581b0f7b6b49d
Suwijuk <suwijuk2261@gmail.com>
2025-03-26 17:32:35 +0700

    จัดการกับ Error

--- actions/actions.ts
+++ actions/actions.ts
@@ -2,16 +2,20 @@
 import { error } from 'console'
 import { profileSchema, validateWithZod } from "@/utils/schemas";
 
-// const profileSchema = z.string().min(2,{message:"อักขระต้องมากกว่า 2 อักขระ"})
+const renderError = (error: unknown): { message: string } => {
+    return {
+        message: error instanceof Error ? error.message  :  'An error server'
+    }
+};
 
-export const createProfile = async (prevState:any, formData: FormData) => {
+export const createProfile = async (prevState: any, formData: FormData) => {
     try {
         const rawData = Object.fromEntries(formData)
-        const validateField = validateWithZod(profileSchema,rawData);
-        console.log('validateField',validateField)
-        return { message : 'Create Profile Success!!!'}
+        const validateField = validateWithZod(profileSchema, rawData);
+        console.log('validateField', validateField)
+        return { message: 'Create Profile Success!!!' }
     } catch {
         console.log(error);
-        return { message: error.message || 'An error server'}
+        return renderError(error)
     }
-  };
+};
